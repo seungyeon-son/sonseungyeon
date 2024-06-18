cc14dc12162ad77a9352c7ca5138ebc0ccc4ecac
syson <au1gust8@gmail.com>
2024-06-18 17:08:46 +0900

    Feat: 라우팅 파일 세팅

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const IndexPage = () => {
+const MainPage = () => {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
@@ -195,25 +195,4 @@ const IndexPage = () => {
     </div>
   );
 };
-export default IndexPage;
-
-function MenuIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
-}
+export default MainPage;
