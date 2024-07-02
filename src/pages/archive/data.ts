e85ecd588599040b211aa9ffd7f35d4bc161590f
syson <au1gust8@gmail.com>
2024-07-02 18:46:58 +0900

    Fix: slug 수정

--- src/pages/archive/data.ts
+++ src/pages/archive/data.ts
@@ -16,28 +16,29 @@ export interface PostsProps {
 export const archivePosts: PostTypes[] = [
   {
     id: 1,
-    slug: "first-post",
+    slug: "1",
     title: "First Post",
-    thumbnail: "https://generated.vusercontent.net/placeholder.svg", // 적절한 이미지 경로로 수정하세요
+    thumbnail:
+      "https://cdn.prod.website-files.com/63340e4175515b8b5cefdbd4/6579c88a686c69936a209fcd_onesignal-supernova-case-study-header.png", // 적절한 이미지 경로로 수정하세요
     description: "This is the description of the first post.",
   },
   {
     id: 2,
-    slug: "second-post",
+    slug: "2",
     title: "Second Post",
     thumbnail: "https://generated.vusercontent.net/placeholder.svg", // 적절한 이미지 경로로 수정하세요
     description: "This is the description of the second post.",
   },
   {
     id: 3,
-    slug: "third-post",
+    slug: "3",
     title: "Third Post",
     thumbnail: "https://generated.vusercontent.net/placeholder.svg", // 적절한 이미지 경로로 수정하세요
     description: "This is the description of the third post.",
   },
   {
     id: 4,
-    slug: "fourth-post",
+    slug: "4",
     title: "Fourth Post",
     thumbnail: "https://generated.vusercontent.net/placeholder.svg", // 적절한 이미지 경로로 수정하세요
     description: "This is the description of the fourth post.",
