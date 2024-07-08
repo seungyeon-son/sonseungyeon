00f581422b7bae6dff5236ccc1c58344928f6811
syson <au1gust8@gmail.com>
2024-07-08 16:52:26 +0900

    Feat: Panel 레이아웃 적용

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,37 @@
 import PageLayout from "@/app/\bcomponents/layout/subLayout";
+import styled from "@emotion/styled";
 
 const AboutPage = () => {
-  return <PageLayout>About Page</PageLayout>;
+  return (
+    <PageLayout>
+      <Panel>
+        <PageHeader>
+          <h1>A. Page</h1>
+        </PageHeader>
+      </Panel>
+    </PageLayout>
+  );
 };
 export default AboutPage;
+
+const PageHeader = styled.div`
+  padding: 208px 0 128px;
+  text-align: center;
+  color: aliceblue;
+  h1 {
+    font-size: 28px;
+    font-weight: 700;
+    margin-bottom: 6px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #bbbbbb;
+  }
+`;
+
+const Panel = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 150svh;
+`;
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,7 +1,38 @@
 import AppLayout from "@/app/\bcomponents/layout/Layout";
 import PageLayout from "@/app/\bcomponents/layout/subLayout";
+import styled from "@emotion/styled";
 
 const ContactPage = () => {
-  return <PageLayout>contact Page</PageLayout>;
+  return (
+    <PageLayout>
+      <Panel>
+        <PageHeader>
+          <h1>C. Page</h1>
+        </PageHeader>
+      </Panel>
+    </PageLayout>
+  );
 };
 export default ContactPage;
+
+const PageHeader = styled.div`
+  padding: 208px 0 128px;
+  text-align: center;
+  color: aliceblue;
+  h1 {
+    font-size: 28px;
+    font-weight: 700;
+    margin-bottom: 6px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #bbbbbb;
+  }
+`;
+
+const Panel = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 150svh;
+`;
