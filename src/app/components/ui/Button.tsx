2e7804230b66e7628bde149f9a0f6c97b72897ee
syson <au1gust8@gmail.com>
2024-07-10 14:49:35 +0900

    Feat: 버튼 컴포넌트 작업

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,127 +1,78 @@
-import styled from "styled-components";
-import { css } from "@emotion/react";
+import styled, { css } from "styled-components";
 
 interface ButtonProps {
   size: "sm" | "md" | "lg";
   variant: "primary" | "secondary" | "ghost" | "white";
   children: React.ReactNode;
-  onClick: () => void;
   value: string;
   disabled: boolean;
 }
 
-const Button = ({ children, onClick, size, variant, ...styleProps }: ButtonProps) => {
-  {
-    return (
-      <ButtonStyle {...styleProps} onClick={onClick} size={size} variant={variant}>
-        <div>{children}</div>
-      </ButtonStyle>
-    );
-  }
+const sizeStyles = {
+  sm: css`
+    width: 50px;
+    height: 30px;
+  `,
+  md: css`
+    width: 150px;
+    height: 30px;
+  `,
+  lg: css`
+    width: 230px;
+    height: 30px;
+  `,
 };
-
-export default Button;
-
-// export const size = {
-//   sm: css`
-//     width: 50px;
-//     height: 30px;
-//   `,
-//   md: css`
-//     width: 150px;
-//     height: 30px;
-//   `,
-//   lg: css`
-//     width: 230px;
-//     height: 30px;
-//   `,
-// };
 
 const ButtonStyle = styled.button<ButtonProps>`
   display: flex;
   align-items: center;
+  justify-content: center;
   overflow: hidden;
   border-radius: 6px;
   font-size: 14px;
   line-height: 1em;
+  ${({ size }) => sizeStyles[size]}
+
   &:hover {
     opacity: 0.9;
   }
   &:disabled {
     opacity: 0.5;
   }
+  ${({ variant }) =>
+    variant === "primary" &&
+    css`
+      background-color: #fff;
+      color: #45434d;
+    `}
+  ${({ variant }) =>
+    variant === "secondary" &&
+    css`
+      background-color: grey;
+      color: white;
+    `}
+  ${({ variant }) =>
+    variant === "ghost" &&
+    css`
+      background-color: transparent;
+      color: black;
+      border: 1px solid black;
+    `}
+  ${({ variant }) =>
+    variant === "white" &&
+    css`
+      background-color: white;
+      color: black;
+      border: 1px solid grey;
+    `}
 `;
 
-/* 
-  ${(ButtonProps) =>
-    ButtonProps.size === "sm" ||
-    css`
-      width: 100px;
-    `}
-  ${(ButtonProps: any) =>
-    ButtonProps.size === "sm" &&
-    css`
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      color: #a1a4aa;
-      font-size: 14px;
-      font-weight: 400;
-      background: none;
-      :hover {
-        background: #3f454f;
-      }
-      :disabled {
-        color: #606467;
-        background: #35393f;
-      }
-    `} */
+const Button = ({ children, size, variant, ...styleProps }: ButtonProps) => {
+  return (
+    <ButtonStyle size={size} variant={variant} {...styleProps}>
+      <div>{children}</div>
+    </ButtonStyle>
+  );
+};
 
-// export const ButtonPrimaryBorder = styled(Button)<ButtonProps>`
-//   height: var(--button-rg);
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   color: ${(props) => props.color || "#fff"};
-//   border: 1px solid;
-//   border-color: ${(props) => props.borderColor || "#eee"};
-//   background: none;
-//   font-size: var(--text-sm);
-//   font-weight: var(--font-400);
-//   border-radius: ${(props) => props.borderradius || "var(--rounded-sm)"};
-//   :hover {
-//     background: ${(props) => props.hoverbgcolor || "none"};
-//   }
-// `;
-
-// export const backButtonColorOfState = {
-//   normal: css`
-//     &:hover {
-//       background-color: #535050;
-//     }
-//   `,
-// };
-
-// export const whiteButtonColorOfState = {
-//   normal: css`
-//     border: 1px solid #7a7979;
-
-//     &:hover {
-//       background-color: #f8f8f8;
-//     }
-//   `,
-// };
-
-// export const brownButtonColorOfState = {
-//   normal: css`
-//     &:hover {
-//       background-color: #5f3837;
-//     }
-//   `,
-// };
-
-// export const buttonType = {
-//   black: backButtonColorOfState,
-//   white: whiteButtonColorOfState,
-//   brown: brownButtonColorOfState,
-// };
+export default Button;
