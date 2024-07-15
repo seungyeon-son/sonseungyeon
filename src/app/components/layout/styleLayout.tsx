5b38504f740e1e6384c076e54ddb6086a7d0d810
syson <au1gust8@gmail.com>
2024-07-15 17:48:15 +0900

    Design: reset css, layout style 추가

---
+++ src/app/components/layout/styleLayout.tsx
@@ -0,0 +1,29 @@
+// import { css } from "styled-components";
+
+// export const HorizontalCenter = css`
+//   display: flex;
+//   justify-content: center;
+// `;
+
+// export const HorizontalCenter2 = css`
+//   position: absolute;
+//   top: 50%;
+//   transform: translateX(-50%);
+// `;
+
+// export const VerticalCenter = css`
+//   display: flex;
+//   align-items: center;
+// `;
+
+// export const VerticalCenter2 = css`
+//   position: absolute;
+//   top: 50%;
+//   transform: translateY(-50%);
+// `;
+
+// export const CenterBoth = css`
+//   display: flex;
+//   justify-content: center;
+//   height: 100vh;
+// `;
